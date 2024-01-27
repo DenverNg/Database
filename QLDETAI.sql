65f7d06fa928203fa8d383a25d8168289c4ad159
Duy Nguyen <nhdduy67@gmail.com>
2024-01-27 22:48:27 +0700

    Finish create table for db qldetai

--- QLDETAI.sql
+++ QLDETAI.sql
@@ -188,6 +188,177 @@ ADD
     FOREIGN KEY(MACD)
     REFERENCES CHUDE
 
+--RANG BUOC MIEN GIA TRI
+ALTER TABLE GIAOVIEN
+ADD CONSTRAINT C_PHAI
+CHECK (PHAI IN (N'Nam', N'Nữ'))
+
+ALTER TABLE NGUOITHAN
+ADD CONSTRAINT C_NT_PHAI
+CHECK (PHAI IN (N'Nam', N'Nữ'))
+
+--NHAP DU LIEU CHO BANG
+INSERT KHOA
+VALUES
+    ('CNTT', N'Công nghệ thông tin', 1995, 'B11', '0838123456', NULL, '02/20/2005'),
+    ('HH', N'Hóa học', 1980, 'B41', '0838456456', NULL, '10/15/2001'),
+    ('SH', N'Sinh học', 1980, 'B31', '0838454545', NULL, '10/11/2000'),
+    ('VL', N'Vật lý', 1976, 'B21', '0838223223', NULL, '09/18/2003')
+
+INSERT BOMON
+VALUES
+    ('CNTT', N'Công nghệ tri thức', 'B15', '0838126126', NULL, 'CNTT', NULL),
+    ('HHC', N'Hóa hữu cơ', 'B44', '838222222', NULL, 'HH', NULL),
+    ('HL', N'Hóa lý', 'B42', '0838878787', NULL, 'HH', NULL),
+    ('HPT', N'Hóa phân tích', 'B43', '0838777777', NULL, 'HH', '10/15/2007'),
+    ('HTTT', N'Hệ thống thông tin', 'B13', '0838125125', NULL, 'CNTT', '09/20/2004'),
+    ('MMT', N'Mạng máy tính', 'B16', '0838676767', NULL, 'CNTT', '05/15/2005'),
+    ('SH', N'Sinh hóa', 'B33', '0838898989', NULL, 'SH', NULL),
+    ('VLƯD', N'Vật lý ứng dụng', 'B24', '0838454545', NULL, 'VL', '02/18/2006'),
+    ('VLĐT', N'Vật lý điện tử', 'B23', '0838234234', NULL, 'VL', NULL),
+    ('VS', N'Vi sinh', 'B32', '0838909090', NULL, 'SH', '01/01/2007')
+
+
+INSERT GIAOVIEN
+VALUES
+    ('001', N'Nguyễn Hoài An', '2000.0', 'Nam', '02/15/1973', N'25/3 Lạc Long Quân, Q.10, TP HCM', NULL, NULL),
+    ('002', N'Trần Trà Hương', '2500.0', N'Nữ', '06/20/1960', N'125 Trần Hưng Đạo, Q.1, TP HCM', NULL, NULL),
+    ('003', N'Nguyễn Ngọc Ánh', '2200.0', N'Nữ', '05/11/1975', N'12/21 Võ Văn Ngân Thủ Đức, TP HCM', NULL, NULL),
+    ('004', N'Trương Nam Sơn', '2300.0', 'Nam', '06/20/1959', N'215 Lý Thường Kiệt, TP Biên Hòa', NULL, NULL),
+    ('005', N'Lý Hoàng Hà', '2500.0', 'Nam', '10/23/1954', N'22/5 Nguyễn Xí, Q.Bình Thạnh, TP HCM', NULL, NULL),
+    ('006', N'Trần Bạch Tuyết', '1500.0', N'Nữ', '05/20/1980', N'127 Hùng Vương, TP Mỹ Tho', NULL, NULL),
+    ('007', N'Nguyễn An Trung', '2100.0', 'Nam', '06/05/1976', N'234 3/2, TP Biên Hòa', NULL, NULL),
+    ('008', N'Trần Trung Hiếu', '1800.0', 'Nam', '08/06/1977', N'22/11 Lý Thường Kiệt, TP Mỹ Tho', NULL, NULL),
+    ('009', N'Trần Hoàng Nam', '2000.0', 'Nam', '11/22/1975', N'234 Trấn Não, An Phú, TP HCM', NULL, NULL),
+    ('010', N'Phạm Nam Thanh', '1500.0', 'Nam', '12/12/1980', N'221 Hùng Vương, Q.5, TP HCM', NULL, NULL)
+
+UPDATE BOMON
+SET MAKHOA='CNTT' WHERE MABM='CNTT'
+UPDATE BOMON
+SET MAKHOA='HH' WHERE MABM='HHC'
+UPDATE BOMON
+SET MAKHOA='HH' WHERE MABM='HL'
+UPDATE BOMON
+SET MAKHOA='HH' WHERE MABM='HPT'
+UPDATE BOMON
+SET MAKHOA='CNTT' WHERE MABM='HTTT'
+UPDATE BOMON
+SET MAKHOA='CNTT' WHERE MABM='MTT'
+UPDATE BOMON
+SET MAKHOA='SH' WHERE MABM='SH'
+UPDATE BOMON
+SET MAKHOA='VL' WHERE MABM='VLĐT'
+UPDATE BOMON
+SET MAKHOA='VL' WHERE MABM='VLƯD'
+UPDATE BOMON
+SET MAKHOA='SH' WHERE MABM='VS'
+
+UPDATE GIAOVIEN
+SET MABM='MMT' WHERE MAGV='001'
+UPDATE GIAOVIEN
+SET MABM='HTTT' WHERE MAGV='002'
+UPDATE GIAOVIEN
+SET GVQLCM='002', MABM='HTTT' WHERE MAGV='003'
+UPDATE GIAOVIEN
+SET MABM='VS' WHERE MAGV='004'
+UPDATE GIAOVIEN 
+SET MABM='VLĐT' WHERE MAGV='005'
+UPDATE GIAOVIEN
+SET GVQLCM='004', MABM='VS' WHERE MAGV='006'
+UPDATE GIAOVIEN
+SET MABM='HPT' WHERE MAGV='007'
+UPDATE GIAOVIEN
+SET GVQLCM='007', MABM='HPT' WHERE MAGV='008'
+UPDATE GIAOVIEN
+SET GVQLCM='001', MABM='MMT' WHERE MAGV='009'
+UPDATE GIAOVIEN
+SET GVQLCM='007', MABM='HPT' WHERE MAGV='010'
+
+UPDATE KHOA
+SET TRUONGKHOA='002' WHERE MAKHOA='CNTT'
+UPDATE KHOA
+SET TRUONGKHOA='007' WHERE MAKHOA='HH'
+UPDATE KHOA
+SET TRUONGKHOA='004' WHERE MAKHOA='SH'
+UPDATE KHOA
+SET TRUONGKHOA='009' WHERE MAKHOA='VL'
+
+UPDATE BOMON
+SET TRUONGBM='007' WHERE MABM='HPT'
+UPDATE BOMON
+SET TRUONGBM='002' WHERE MABM='HTTT'
+UPDATE BOMON
+SET TRUONGBM='001' WHERE MABM='MMT'
+
+INSERT GV_DT
+VALUES
+    ('001', '0838912112'),
+    ('001', '0903123123'),
+    ('002', '0913454545'),
+    ('003', '0838121212'),
+    ('003', '0903656565'),
+    ('003', '0937125125'),
+    ('006', '0937888888'),
+    ('008', '0653717171'),
+    ('008', '0913232323')
+
+INSERT CHUDE
+VALUES
+    ('NCPT', N'Nghiên cứu phát triển'),
+    ('QLGD', N'Quản lý giáo dục'),
+    ('ƯDCN', N'Ứng dụng công nghệ')
+
+INSERT DETAI
+VALUES
+    ('001', N'HTTT quản lý các trường ĐH', 'ĐHQG', '20.0', '10/20/2007', '10/20/2008', 'QLGD', '002'),
+    ('002', N'HTTT quản lý giáo vụ cho một Khoa', N'Trường', '20.0', '10/12/2000', '10/12/2001', 'QLGD', '002'),
+    ('003', N'Nghiên cứu chế tạo sợi Nanô Platin', 'ĐHQG', '300.0', '05/15/2008', '05/15/2010', 'NCPT', '005'),
+    ('004', N'Tạo vật liệu sinh học bằng màng ối người', N'Nhà nước', '100.0', '01/01/2003', '12/31/2008', 'NCPT', '004'),
+    ('005', N'Ứng dụng hóa học xanh', N'Trường', '200.0', '10/10/2003', '12/10/2004', 'ƯDCN', '007'),
+    ('006', N'Nghiên cứu tế bào gốc', N'Nhà nước', '4000.0', '10/20/2006', '10/20/2009', 'NCPT', '004'),
+    ('007', N'HTTT quản lý thư viện ở các trường ĐH', N'Trường', '20.0', '05/10/2009', '05/10/2010', 'QLGD', '001')
+
+INSERT CONGVIEC
+VALUES
+    ('001', 1, N'Khởi tạo và Lập kế hoạch', '10/20/2007', '12/20/2008'),
+    ('001', 2, N'Xác định yêu cầu', '12/21/2008', '03/21/2008'),
+    ('001', 3, N'Phân tích hệ thống', '03/22/2008', '05/22/2008'),
+    ('001', 4, N'Thiết kế hệ thống', '05/23/2008', '06/23/2008'),
+    ('001', 5, N'Cài đặt thử nghiệm', '06/24/2008', '10/20/2008'),
+    ('002', 1, N'Khởi tạo và Lập kế hoạch', '05/10/2009', '07/10/2009'),
+    ('002', 2, N'Xác định yêu cầu', '07/11/2009', '10/11/2009'),
+    ('002', 3, N'Phân tích hệ thống', '10/12/2009', '12/20/2009'),
+    ('002', 4, N'Thiết kế hệ thống', '12/21/2009', '03/22/2010'),
+    ('002', 5, N'Cài đặt thử nghiệm', '03/23/2010', '05/10/2010'),
+    ('006', 1, N'Lấy mẫu', '10/20/2006', '02/20/2007'),
+    ('006', 2, N'Nuôi cấy', '02/21/2007', '08/21/2008')
+
+INSERT THAMGIADT
+VALUES
+    ('001', '002', 1, '0.0', NULL),
+    ('001', '002', 2, '2.0', NULL),
+    ('002', '001', 4, '2.0', N'Đạt'),
+    ('003', '001', 1, '1.0', N'Đạt'),
+    ('003', '001', 2, '0.0', N'Đạt'),
+    ('003', '001', 4, '1.0', N'Đạt'),
+    ('003', '002', 2, '0.0', NULL),
+    ('004', '006', 1, '0.0', N'Đạt'),
+    ('004', '006', 2, '1.0', N'Đạt'),
+    ('006', '006', 2, '1.5', N'Đạt'),
+    ('009', '002', 3, '0.5', NULL),
+    ('009', '002', 4, '1.5', NULL)
+
+INSERT NGUOITHAN
+VALUES
+    ('001', N'Hùng', '01/14/1990', 'Nam'),
+    ('001', N'Thủy', '12/08/1994', N'Nữ'),
+    ('003', N'Hà', '09/03/1998', N'Nữ'),
+    ('003', 'Thu', '09/03/1998', N'Nữ'),
+    ('007', 'Mai', '03/26/2003', N'Nữ'),
+    ('007', 'Vy', '02/14/2000', N'Nữ'),
+    ('008', 'Nam', '05/06/1991', 'Nam'),
+    ('009', 'An', '08/19/1996', 'Nam'),
+    ('010', N'Nguyệt', '01/14/2006', N'Nữ')
 
 SELECT*
 FROM GIAOVIEN
